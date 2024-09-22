6f3f8652d3bbadaa5a1c3532bb3fc77dc9d24bfa
notjustdevops <Not.Just.DevOps@gmail.com>
2024-09-22 22:45:43 +0300

    Testing how to solve: docker logs keplet6

--- Dvorkin-Guy-From-Kepler-186f-App/src/main/java/com/myapp/App.java
+++ Dvorkin-Guy-From-Kepler-186f-App/src/main/java/com/myapp/App.java
@@ -6,6 +6,6 @@ package com.myapp;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World, ...Auto Replaced in CI Job");
+        System.out.println("Hello World, ...Auto Replaced in CI Job v2");
     }
 }
