2306ac7f3a011cb1a184bbe031d6e30eb84c82e1
notjustdevops <Not.Just.DevOps@gmail.com>
2024-09-23 12:28:42 +0300

    Solving the Version Increment Issue

--- Dvorkin-Guy-From-Kepler-186f-App/src/main/java/com/myapp/App.java
+++ Dvorkin-Guy-From-Kepler-186f-App/src/main/java/com/myapp/App.java
@@ -1,3 +1,5 @@
+// path: Dvorkin-Guy-From-Kepler-186f-App/src/main/java/com/myapp/App.java
+
 package com.myapp;
 
 import static spark.Spark.*;
@@ -16,8 +18,8 @@ public class App {
         System.out.println("Server is running on http://localhost:8080/");
     }
 
-    // Method that returns the greeting message
+    // Method that returns the greeting message with updated version string
     public static String getGreeting() {
-        return "Hello World, this is Dvorkin Guy v1.0.2";  // Update version string
+        return "Hello World, this is Dvorkin Guy v1.0.3!";  // Increment the version here
     }
 }
